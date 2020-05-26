234cd89bf66dc886bb5fb4284c6a7962fbdf50e9
LeelK <karunarathne1995@gmail.com>
2020-05-26 22:13:22 +0530

    Role base Authentication with Annotations

--- src/main/java/com/ijse/dep/pos/controller/StudentManagementController.java
+++ src/main/java/com/ijse/dep/pos/controller/StudentManagementController.java
@@ -1,6 +1,7 @@
 package com.ijse.dep.pos.controller;
 
 import com.ijse.dep.pos.Entity.Student;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -17,24 +18,29 @@ public class StudentManagementController {
             new Student(3,"Sahan")
     );
 
+
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents()
     {
         return  STUDENTS;
     }
 
     @PostMapping
+    @PreAuthorize("hasAnyAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student){
         System.out.println(student);
     }
 
     @DeleteMapping(path = "{studentID}")
+    @PreAuthorize("hasAnyAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentID") Integer studentID){
         System.out.println(studentID);
     }
 
 
     @PutMapping(path ="{studentID}")
+    @PreAuthorize("hasAnyAuthority('student:write')")
     public void updateStudent(@PathVariable("studentID")Integer studentid, @RequestBody Student student){
         System.out.println("Updating");
     }
